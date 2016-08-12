32b757889b9ffaaf7f0d860d46452de2348f9512
Allan L. R. Hansen <allan.lrh@gmail.com>
2016-08-12 22:43:11 +0200

    Added commad line interface to mergeNotebooks.py.

--- mergeNotebooks.py
+++ mergeNotebooks.py
@@ -1,22 +1,47 @@
 #!/usr/bin/env python
 
-import json
-from copy import deepcopy
+from splitNotebook import loadNotebook, writeNotebook
+import argparse
 
-inputnameLst = ['triaNotebook_input.ipynb', 'triaNotebook_output.ipynb']
-with open(inputnameLst[0]) as fid:
-    nbi = json.load(fid)
-with open(inputnameLst[1]) as fid:
-    nbo = json.load(fid)
 
-nbc = deepcopy(nbi)
-for cc, co in zip(nbc['cells'], nbo['cells']):
-    if 'execution_count' in co:
-        cc['execution_count'] = co['execution_count']
-    if 'outputs' in co:
-        cc['outputs'] = co['outputs']
+def mergeNotebooks(nbi, nbo):
+    """
+    Merged the notebook with the input data (nbi) and notebook with output data (nbo).
+    """
+    for cc, co in zip(nbi['cells'], nbo['cells']):
+        if 'execution_count' in co:
+            cc['execution_count'] = co['execution_count']
+        if 'outputs' in co:
+            cc['outputs'] = co['outputs']
+    return nbi  # updated with merged data
 
-nbon = 'trialNotebook_combined.ipynb'
-with open(nbon, 'w') as fid:
-    json.dump(nbc, fid)
 
+def getCommonSubstring(a, b):
+    """
+    Starting from index 0, return the common substring for a and b.
+    """
+    ret = list()
+    for i, j in zip(a, b):
+        if i == j:
+            ret.append(i)
+        else:
+            return ''.join(ret)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("MergeNotebook")
+    parser.add_argument('inputNotebook', help="Notebook with input data.")
+    parser.add_argument('outputNotebook', help="Notebook with output data.")
+    parser.add_argument('-o', '--outputname', help="Filename of merged notebook.",
+                        type=str, default=None)
+    p = parser.parse_args()
+
+    nbi = loadNotebook(p.inputNotebook)
+    nbo = loadNotebook(p.outputNotebook)
+    nbm = mergeNotebooks(nbi, nbo)
+    if p.outputname is None:
+        outputname = getCommonSubstring(p.inputNotebook, p.outputNotebook)
+        outputname = outputname + '_merged.ipynb' if len(outputname) else 'mergedNotebook.ipynb'
+    else:
+        outputname = p.outputname
+    writeNotebook(nbm, outputname)
