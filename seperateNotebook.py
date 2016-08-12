2eb44d52de5b966ba07a6514f8a191c17ad03ec9
Allan L. R. Hansen <allan.lrh@gmail.com>
2016-08-12 16:13:22 +0200

    seperateNotebooks.py is working with user friendly command line interface

--- seperateNotebook.py
+++ seperateNotebook.py
@@ -2,30 +2,73 @@
 
 import json
 from copy import deepcopy
+import argparse
 
-inputname = 'triaNotebook.ipynb'
-with open(inputname) as fid:
-    nbo = json.load(fid)
-nbi = deepcopy(nbo)
-nbo = deepcopy(nbo)
 
-for cl in nbi['cells']:
-    if 'outputs' in cl.keys():
-        cl['outputs'] = []
-    if 'execution_count' in cl.keys():
-        cl['execution_count'] = None
+def loadNotebook(filename):
+    """
+    Load notebook as json structure.
+    """
+    with open(filename) as fid:
+        nb = json.load(fid)
+        return nb
 
-for cl in nbo['cells']:
-    if 'source' in cl.keys():
-        cl['source'] = ''
 
-nbis = json.dumps(nbi)
-nbos = json.dumps(nbo)
-a, b = inputname.split('.')
-nbin = a + '_input.' + b
-nbon = a + '_output.' + b
+def splitNotebook(jsonbook):
+    """
+    Split notebook (json structure) into two structures,
+    one with the inputs and one with the outputs.
+    """
+    nbi = deepcopy(jsonbook)  # Notebook with inputs
+    nbo = deepcopy(jsonbook)  # Notebook with outputs
+    for cl in nbi['cells']:
+        if 'outputs' in cl.keys():
+            cl['outputs'] = []
+        if 'execution_count' in cl.keys():
+            cl['execution_count'] = None
 
-with open(nbin, 'w') as fid:
-    fid.write(nbis)
-with open(nbon, 'w') as fid:
-    fid.write(nbos)
+    for cl in nbo['cells']:
+        if 'source' in cl.keys():
+            cl['source'] = ''
+    return (nbi, nbo)
+
+
+def makeNewNames(filename):
+    """
+    Append '_input' and '_output' to the end of the filename,
+    but before the file-extension.
+    """
+    a, b = filename.split('.')
+    nbin = a + '_input.' + b
+    nbon = a + '_output.' + b
+    return (nbin, nbon)
+
+
+def writeNotebook(jsonbook, filename):
+    with open(filename, 'w') as fid:
+        json.dump(jsonbook, fid)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("SplitNotebook")
+    parser.add_argument('filename', help="Name of the notebook to be split")
+    parser.add_argument('-i', '--nooutputnotebook', help="Don't make notebook containing output",
+                        action='store_true', default=False)
+    parser.add_argument('-o', '--outputnames',
+                        help="Name(s) of output notebooks, provide names for both"
+                        "notebooks unless the --nooutputnotebook flag is given.",
+                        type=str, nargs='+')
+    p = parser.parse_args()
+
+    nb = loadNotebook(p.filename)
+    nbi, nbo = splitNotebook(nb)
+    if not p.outputnames:
+        inpName, outName = makeNewNames(p.filename)
+    else:
+        if p.nooutputnotebook:
+            inpName = p.outputnames[0]
+        else:
+            inpName, outName = p.outputnames
+    writeNotebook(nbi, inpName)
+    if not p.nooutputnotebook:
+        writeNotebook(nbo, outName)
